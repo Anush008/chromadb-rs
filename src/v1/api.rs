425d1d02a4588053835895c41587ad8e28b74935
Anush008 <46051506+Anush008@users.noreply.github.com>
2023-09-10 19:40:25 +0530

    refactor: api.rs DRY

--- src/v1/api.rs
+++ src/v1/api.rs
@@ -10,85 +10,48 @@ pub(super) struct APIClientV1 {
 impl APIClientV1 {
     pub fn new(endpoint: String) -> Self {
         Self {
-            api_endpoint: endpoint + "/api/v1",
+            api_endpoint: format!("{}/api/v1", endpoint),
         }
     }
 
     pub fn post(&self, path: &str, json_body: Option<Value>) -> Result<Response> {
-        let url = format!(
-            "{api_endpoint}{path}",
-            api_endpoint = self.api_endpoint,
-            path = path
-        );
-
-        let res = minreq::post(url)
-            .with_header("Content-Type", "application/json")
-            .with_json(&json_body)?
-            .send()?;
-
-        match res.status_code {
-            200..=299 => Ok(res),
-            _ => anyhow::bail!(
-                "{} {}: {}",
-                res.status_code,
-                res.reason_phrase,
-                res.as_str().unwrap()
-            ),
-        }
+        self.send_request("POST", path, json_body)
     }
 
     pub fn get(&self, path: &str) -> Result<Response> {
-        let url = format!(
-            "{api_endpoint}{path}",
-            api_endpoint = self.api_endpoint,
-            path = path
-        );
-        let res = minreq::get(url).send()?;
-        match res.status_code {
-            200..=299 => Ok(res),
-            _ => anyhow::bail!(
-                "{} {}: {}",
-                res.status_code,
-                res.reason_phrase,
-                res.as_str().unwrap()
-            ),
-        }
+        self.send_request("GET", path, None)
     }
 
     pub fn put(&self, path: &str, json_body: Option<Value>) -> Result<Response> {
-        let url = format!(
-            "{api_endpoint}{path}",
-            api_endpoint = self.api_endpoint,
-            path = path
-        );
-
-        let json_body = match json_body {
-            Some(json_body) => serde_json::to_value(json_body).unwrap(),
-            None => Value::Null,
-        };
-
-        let res = minreq::put(url)
-            .with_header("Content-Type", "application/json")
-            .with_json(&json_body)?
-            .send()?;
-        match res.status_code {
-            200..=299 => Ok(res),
-            _ => anyhow::bail!(
-                "{} {}: {}",
-                res.status_code,
-                res.reason_phrase,
-                res.as_str().unwrap()
-            ),
-        }
+        self.send_request("PUT", path, json_body)
     }
 
     pub fn delete(&self, path: &str) -> Result<Response> {
+        self.send_request("DELETE", path, None)
+    }
+
+    fn send_request(&self, method: &str, path: &str, json_body: Option<Value>) -> Result<Response> {
         let url = format!(
             "{api_endpoint}{path}",
             api_endpoint = self.api_endpoint,
             path = path
         );
-        let res = minreq::delete(url).send()?;
+
+        let request = match method {
+            "POST" => minreq::post(url),
+            "PUT" => minreq::put(url),
+            "DELETE" => minreq::delete(url),
+            _ => minreq::get(url),
+        };
+
+        let request = if let Some(body) = json_body {
+            request.with_header("Content-Type", "application/json").with_json(&body)?
+        } else {
+            request
+        };
+
+        let res = request.send()?;
+
         match res.status_code {
             200..=299 => Ok(res),
             _ => anyhow::bail!(
